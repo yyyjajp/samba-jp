551308e88656b9e6d2bbdf27b0fdb3306ffc8509
ribbon <ribbon@b25e73f6-4969-4fba-a6af-f736fb73aa4f>
2009-04-14 08:25:12 +0000

    update setupjp32.sh

--- docs-xml/setupjp32.sh
+++ docs-xml/setupjp32.sh
@@ -4,7 +4,7 @@ cp -pvr 3.2/docs-xml/manpages-3/* docs-xml/manpages-3
 cp -pvr 3.2/docs-xml/smbdotconf/* docs-xml/smbdotconf
 cp -pv  3.2/docs-xml/build/DTD/samba.entities docs-xml/build/DTD/
 cp -pv  3.2/docs-xml/xslt/expand-smbconfdoc.xsl docs-xml/xslt/
-cat $(pwd)/docs-xml/build/catalog.xml.in | sed -e "s|@BUILDDIR@|$(pwd)|g" > $(pwd)/docs-xml/build/catalog.xml
+cat $(pwd)/docs-xml/build/catalog.xml.in | sed -e "s|@BUILDDIR@|$(pwd)/docs-xml|g" > $(pwd)/docs-xml/build/catalog.xml
 mv  $(pwd)/docs-xml/build/catalog.xml.in  $(pwd)/docs-xml/build/catalog.xml.in.org
 mv  $(pwd)/docs-xml/build/catalog.xml     $(pwd)/docs-xml/build/catalog.xml.in
 cd docs-xml
