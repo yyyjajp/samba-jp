366031c1acb959dd2471b26b3a607bc70dd8decb
ribbon <ribbon@b25e73f6-4969-4fba-a6af-f736fb73aa4f>
2009-07-17 00:47:27 +0000

    update html,manpage files

---
+++ docs-xml/setupjp33.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+svn export https://svn.sourceforge.jp/svnroot/samba-jp/branches/3.3
+cp -pvr 3.3/docs-xml/manpages-3/* docs-xml/manpages-3/
+cp -pvr 3.3/docs-xml/smbdotconf/* docs-xml/smbdotconf/
+cp -pv  3.3/docs-xml/build/DTD/samba.entities docs-xml/build/DTD/
+cp -pv  3.3/docs-xml/xslt/expand-smbconfdoc.xsl docs-xml/xslt/
+cat $(pwd)/docs-xml/build/catalog.xml.in | sed -e "s|@BUILDDIR@|$(pwd)/docs-xml|g" > $(pwd)/docs-xml/build/catalog.xml
+mv  $(pwd)/docs-xml/build/catalog.xml.in  $(pwd)/docs-xml/build/catalog.xml.in.org
+mv  $(pwd)/docs-xml/build/catalog.xml     $(pwd)/docs-xml/build/catalog.xml.in
+cd docs-xml
+./configure
+make smbdotconf/parameters.all.xml
+
