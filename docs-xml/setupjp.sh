7237d3bdefe7ac9029ca683de6b653f0fadf7ae0
ribbon <ribbon@b25e73f6-4969-4fba-a6af-f736fb73aa4f>
2010-01-05 02:58:40 +0000

    add error handling

--- docs-xml/setupjp.sh
+++ docs-xml/setupjp.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
-svn export svn+ssh://svn.sourceforge.jp/svnroot/samba-jp/trunc
+svn export https://svn.sourceforge.jp/svnroot/samba-jp/trunc
+if [ $? -ne 0 ]
+then
+   echo svn get error
+   exit 1
+fi
 cp -pvr trunc/docs-xml/manpages-3/* docs-xml/manpages-3
 cp -pvr trunc/docs-xml/smbdotconf/* docs-xml/smbdotconf
+cp -pvr trunc/docs-xml/Samba3-HOWTO/* docs-xml/Samba3-HOWTO
 cp -pv  trunc/docs-xml/build/DTD/samba.entities docs-xml/build/DTD/
 cp -pv  trunc/docs-xml/xslt/expand-smbconfdoc.xsl docs-xml/xslt/
 cat $(pwd)/docs-xml/build/catalog.xml.in | sed -e "s|@BUILDDIR@|$(pwd)/docs-xml|g" > $(pwd)/docs-xml/build/catalog.xml
