e8bceee48b4aa6fa6f38e1f18d48fb3bdbb6c980
ribbon <ribbon@b25e73f6-4969-4fba-a6af-f736fb73aa4f>
2010-02-12 10:59:01 +0000

    update setupjp34.sh

--- docs-xml/setupjp34.sh
+++ docs-xml/setupjp34.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
-svn export https://svn.sourceforge.jp/svnroot/samba-jp/trunc
-if [ $? -ne 0 ]
-then
-   echo svn get error
-   exit 1
-fi
-cp -pvr trunc/docs-xml/manpages-3/* docs-xml/manpages-3
-cp -pvr trunc/docs-xml/smbdotconf/* docs-xml/smbdotconf
-cp -pvr trunc/docs-xml/Samba3-HOWTO/* docs-xml/Samba3-HOWTO
-cp -pv  trunc/docs-xml/build/DTD/samba.entities docs-xml/build/DTD/
-cp -pv  trunc/docs-xml/xslt/expand-smbconfdoc.xsl docs-xml/xslt/
+svn export https://svn.sourceforge.jp/svnroot/samba-jp/branches/3.4
+cp -pvr 3.4/docs-xml/manpages-3/* docs-xml/manpages-3/
+cp -pvr 3.4/docs-xml/smbdotconf/* docs-xml/smbdotconf/
+cp -pv  3.4/docs-xml/build/DTD/samba.entities docs-xml/build/DTD/
+cp -pv  3.4/docs-xml/xslt/expand-smbconfdoc.xsl docs-xml/xslt/
 cat $(pwd)/docs-xml/build/catalog.xml.in | sed -e "s|@BUILDDIR@|$(pwd)/docs-xml|g" > $(pwd)/docs-xml/build/catalog.xml
 mv  $(pwd)/docs-xml/build/catalog.xml.in  $(pwd)/docs-xml/build/catalog.xml.in.org
 mv  $(pwd)/docs-xml/build/catalog.xml     $(pwd)/docs-xml/build/catalog.xml.in
-
+cd docs-xml
+./configure
+make smbdotconf/parameters.all.xml
