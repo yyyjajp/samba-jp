db9db135c2f65800eded6ec7726f4d16eb1807b0
ribbon <ribbon@b25e73f6-4969-4fba-a6af-f736fb73aa4f>
2010-02-19 05:54:15 +0000

    update script

--- docs-xml/conv.sh
+++ docs-xml/conv.sh
@@ -1,8 +1,10 @@
 #! /bin/sh
-cd output/htmldocs/Samba3-HOWTO
+cd output/htmldocs/manpages-3
 for file in *.html
 do
    echo $file
    php ../../../htmlconv.php <$file >$file.new
+   mv $file $file.old
+   mv $file.new $file
 done
 
