f8b9a65f369f1b2e28f1c5754c394f04805d6425
ribbon <ribbon@b25e73f6-4969-4fba-a6af-f736fb73aa4f>
2010-03-21 14:19:01 +0000

    update setupjp32.sh (xslt directory full copy)

--- docs-xml/setupjp32.sh
+++ docs-xml/setupjp32.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-svn export svn+ssh://svn.sourceforge.jp/svnroot/samba-jp/branches/3.2
+svn export https://svn.sourceforge.jp/svnroot/samba-jp/branches/3.2
 cp -pvr 3.2/docs-xml/manpages-3/* docs-xml/manpages-3
 cp -pvr 3.2/docs-xml/smbdotconf/* docs-xml/smbdotconf
 cp -pv  3.2/docs-xml/build/DTD/samba.entities docs-xml/build/DTD/
-cp -pv  3.2/docs-xml/xslt/expand-smbconfdoc.xsl docs-xml/xslt/
+cp -pv  3.2/docs-xml/xslt/* docs-xml/xslt/
 cat $(pwd)/docs-xml/build/catalog.xml.in | sed -e "s|@BUILDDIR@|$(pwd)/docs-xml|g" > $(pwd)/docs-xml/build/catalog.xml
 mv  $(pwd)/docs-xml/build/catalog.xml.in  $(pwd)/docs-xml/build/catalog.xml.in.org
 mv  $(pwd)/docs-xml/build/catalog.xml     $(pwd)/docs-xml/build/catalog.xml.in
