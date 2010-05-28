529bcac0799ccdf00f3dbf1895b1377c0e445026
ribbon <ribbon@b25e73f6-4969-4fba-a6af-f736fb73aa4f>
2010-05-28 06:53:31 +0000

    update setupjp.sh

--- docs-xml/setupjp.sh
+++ docs-xml/setupjp.sh
@@ -8,6 +8,7 @@ fi
 cp -pvr trunc/docs-xml/manpages-3/* docs-xml/manpages-3
 cp -pvr trunc/docs-xml/smbdotconf/* docs-xml/smbdotconf
 cp -pvr trunc/docs-xml/Samba3-HOWTO/* docs-xml/Samba3-HOWTO
+cp -pvr trunc/docs-xml/Samba3-Developers-Guide/* docs-xml/Samba3-Developers-guide
 cp -pv  trunc/docs-xml/build/DTD/samba.entities docs-xml/build/DTD/
 cp -pv  trunc/docs-xml/xslt/expand-smbconfdoc.xsl docs-xml/xslt/
 cat $(pwd)/docs-xml/build/catalog.xml.in | sed -e "s|@BUILDDIR@|$(pwd)/docs-xml|g" > $(pwd)/docs-xml/build/catalog.xml
