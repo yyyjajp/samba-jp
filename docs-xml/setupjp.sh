8f68e061fd8365c4a30434a653a42e0b2b1f7b00
fumiyas <fumiyas@b25e73f6-4969-4fba-a6af-f736fb73aa4f>
2010-12-10 11:36:13 +0000

    s/trunc/trunk/

--- docs-xml/setupjp.sh
+++ docs-xml/setupjp.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-svn export https://svn.sourceforge.jp/svnroot/samba-jp/trunc
+svn export https://svn.sourceforge.jp/svnroot/samba-jp/trunk
 if [ $? -ne 0 ]
 then
    echo svn get error
    exit 1
 fi
-cp -pvr trunc/docs-xml/manpages-3/* docs-xml/manpages-3
-cp -pvr trunc/docs-xml/smbdotconf/* docs-xml/smbdotconf
-cp -pvr trunc/docs-xml/Samba3-HOWTO/* docs-xml/Samba3-HOWTO
-cp -pvr trunc/docs-xml/Samba3-Developers-Guide/* docs-xml/Samba3-Developers-Guide
-cp -pv  trunc/docs-xml/build/DTD/samba.entities docs-xml/build/DTD/
-cp -pv  trunc/docs-xml/xslt/* docs-xml/xslt/
+cp -pvr trunk/docs-xml/manpages-3/* docs-xml/manpages-3
+cp -pvr trunk/docs-xml/smbdotconf/* docs-xml/smbdotconf
+cp -pvr trunk/docs-xml/Samba3-HOWTO/* docs-xml/Samba3-HOWTO
+cp -pvr trunk/docs-xml/Samba3-Developers-Guide/* docs-xml/Samba3-Developers-Guide
+cp -pv  trunk/docs-xml/build/DTD/samba.entities docs-xml/build/DTD/
+cp -pv  trunk/docs-xml/xslt/* docs-xml/xslt/
 cat $(pwd)/docs-xml/build/catalog.xml.in | sed -e "s|@BUILDDIR@|$(pwd)/docs-xml|g" > $(pwd)/docs-xml/build/catalog.xml
 mv  $(pwd)/docs-xml/build/catalog.xml.in  $(pwd)/docs-xml/build/catalog.xml.in.org
 mv  $(pwd)/docs-xml/build/catalog.xml     $(pwd)/docs-xml/build/catalog.xml.in
